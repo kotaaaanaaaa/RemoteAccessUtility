85a1a4741749317caea1a59166c6bffbcfaf69e6
kota_na_ <ad.maiora.1@gmail.com>
2019-04-06 23:59:02 +0900

    feat: 単体テスト chore: ソリューションを2019に

---
+++ RemoteAccessUtility.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RemoteAccessUtility.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("RemoteAccessUtility.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db3b6aeb-12b5-4135-a2b5-5a17f3155a42")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RemoteAccessUtility.Test/RdpOptionTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RemoteAccessUtility.Test
+{
+    [TestClass]
+    public class RdpOptionTest
+    {
+        [TestMethod]
+        public void EncryptDecrypt1()
+        {
+            var expected = "password";
+
+            var encryptData = DpApiAccessor.Encrypt(expected);
+            var actual = DpApiAccessor.Decrypt(encryptData);
+            Assert.AreEqual(expected, actual);
+
+            encryptData = DpApiAccessor.Encrypt(DpApiAccessor.KeyType.UserKey, expected);
+            actual = DpApiAccessor.Decrypt(encryptData);
+            Assert.AreEqual(expected, actual);
+
+            encryptData = DpApiAccessor.Encrypt(DpApiAccessor.KeyType.MachineKey, expected, string.Empty, expected);
+            actual = DpApiAccessor.Decrypt(encryptData, out var description);
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, description);
+
+            encryptData = DpApiAccessor.Encrypt(DpApiAccessor.KeyType.MachineKey, expected, expected, expected);
+            actual = DpApiAccessor.Decrypt(encryptData, expected, out description);
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected, description);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void EncryptDecrypt2()
+        {
+            var expected = "password";
+
+            var encryptData = DpApiAccessor.Encrypt(DpApiAccessor.KeyType.MachineKey, expected, expected, expected);
+            DpApiAccessor.Decrypt(encryptData, out var description);
+        }
+    }
+}
